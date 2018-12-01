231fdfdfd552ed13febb582e934c5fdb378e6098
Herbert Poul <herbert@poul.at>
2018-12-01 21:24:19 +0200

    basic project setup with logging.

--- src/main/kotlin/design/codeux/coroutine/playground/CoroutineExample.kt
+++ src/main/kotlin/design/codeux/coroutine/playground/CoroutineExample.kt
@@ -1,5 +1,18 @@
 package design.codeux.coroutine.playground
 
+import mu.KotlinLogging
+import org.slf4j.bridge.SLF4JBridgeHandler
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>) {
-    println("Lorem ipsum.")
+    initLogging()
+
+    logger.info { "Starting playground ..." }
+}
+
+fun initLogging() {
+    // install JUL to slf4j bridge (java util logging)
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
 }
